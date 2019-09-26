bfad7271a5ece1bbfbd8c31ef2cedfa4ababd11e
Frederik Høeg <frederik@skitze.dk>
2019-09-26 13:13:27 +0200

    Første skitse til de første par klasser

--- main.js
+++ main.js
@@ -4,13 +4,49 @@ class Revisorhus {
         this.revisore = [];
     }
 
+
+    //Becuase objects are mutable in JS, we can simply add a Revisor object to the
+    //Revisorhus and in that way, a change at the revisor level (or Meeting level) will result in
+    //the same change on the Revisorhus level
     addRevisor(navn) {
-        this.revisore.append(navn);
+        //Tag et objekt som argument, og gem tider osv. i dette objekt
+        //Ved at gemme i et objekt, kan vi lave ændringer i objektet, og
+        //på den måde, nemt lave ændringer
+
+        this.revisore.push(navn);
+    }
+    printInfo(){
+        console.log("Revisorhusinfo\nNavn: " + this.revisorhusNavn + "\nIndeholdere revisore: ");
+        for(var i=0; i<this.revisore.length; i++){
+            this.revisore[i].printInfo();
+        }
     }
 }
 
+class Revisor {
+    constructor (navn) {
+        this.revisorNavn = navn;
+        this.meetings = [];
+    }
+    addMeeting (m) {
+        console.log(this.meetings);
+        this.meetings.push(m);
+
+        //TODO:
+        //Gem mødet i local storage
+    }
+    printInfo(){
+        console.log("Revisorinfo\nNavn: " + this.revisorNavn + "\nMeetings:");
+        for (var i=0; i<this.meetings.length; i++){
+            console.log("Meeting number " + i + " starts at: " + this.meetings[i].startTime + " and end at: " + this.meetings[i].endTime);
+        }
+    }
+}
+
+
+
 class Meeting {
-    constructor (start, slut){
+    constructor (start, slut) {
         this.startTime = start;
         this.endTime = slut;
     }
@@ -44,11 +80,29 @@ var start = new Date(today.getFullYear(), today.getMonth(), today.getDate(), 11,
 var slut = new Date(start.getTime() + 60 * 60 * 1000);
 
 meeting1 = new Meeting(start, slut);
-meeting1.printTime();
-meeting1.printMeetingDuration();
 
 
 var startMoms = new Date(today.getFullYear(), today.getMonth(), today.getDate(), 15, 0, 0, 0);
 moms = new MomsRegnskab(startMoms);
-moms.printTime();
-moms.printMeetingDuration();
+
+
+//Lave revisor
+peter = new Revisor("Peter");
+peter.addMeeting(moms);
+peter.addMeeting(meeting1);
+//peter.printInfo();
+
+//Lav revisorhus
+revisorCentralen = new Revisorhus('Revisorcentralen');
+revisorCentralen.addRevisor(peter);
+revisorCentralen.printInfo();
+
+//peter.revisorNavn = 'John';
+//revisorCentralen.printInfo();
+
+
+function loadData(){
+    //TODO
+    //Skal kunne loade data fra både arrays og fra local data
+    //Se: https://stackoverflow.com/a/2010948
+}
