6627b2e93cb4dcd931d07b24de685ce78210a2c6
anda19ad <anda19ad@student.cbs.dk>
2019-10-14 10:24:08 +0200

    Revisorhus

--- classRevisorhus.js
+++ classRevisorhus.js
@@ -4,20 +4,22 @@ class Revisorhus {
         this.revisorhusInfo = revisorhusInfo;
         this.revisorer = [];
     }
+    //For at kunne ændre i nanvet på revisorhuset, tilføjes en get function
     getRevisorhusInfo() {
         return this.revisorhusInfo;
     }
-
+    //For at kunne ændre i navnet tilføjes get en get function
     getRevisorer() {
         return this.revisorer;
     }
-
+    //Laver en function for at kunne tilføje flere revisorer uden for denne klasse
     addRevisor(revisor) {
         this.revisorer.push(revisor);
     }
 
 }
 
+//Ti
 let revhus = new Revisorhus();
 revhus.setRevisorhus("Birkholm Revision");
 
