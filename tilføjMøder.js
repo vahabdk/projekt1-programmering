f27074c1cb3dd2c7e803e05ee29c8643e1f55077
vahabdk <varo10ab@student.cbs.dk>
2019-10-18 08:31:46 +0200

    classes

---
+++ tilføjMøder.js
@@ -0,0 +1,7 @@
+function tilføjMøder(){
+    var startTime = document.getElementById("startTime").value;
+    var endTime = document.getElementById("endtime").value;
+    var kundenavn = document.getElementById("kundenavn").value;
+    var kommentar = document.getElementById("kommentar").value;
+
+}
