64d850eac4cf5e2b727940e235e0cfc0bf7828f9
anda19ad <anda19ad@student.cbs.dk>
2019-10-18 08:10:42 +0200

    Tilføjet id

---
+++ OpretBruger.js
@@ -0,0 +1,11 @@
+function kontrolInfo(){
+    var Fornavn = document.getElementById(Fornavn).value;
+    var Efternavn = document.getElementById(Efternavn).value;
+    var Email = document.getElementById(Email).value;
+    var startTime = document.getElementById(startTime);
+    var startMinut = document.getElementById(startMinut);
+    var slutTime = document.getElementById(slutTime);
+    var slutMinut = document.getElementById(slutMinut);
+    var Brugernavn = document.getElementById(Brugernavn)
+
+}
