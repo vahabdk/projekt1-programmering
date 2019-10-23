7b2be6ce2e2b292574ed91dd1a12bd23a038d915
anda19ad <anda19ad@student.cbs.dk>
2019-10-23 14:21:47 +0200

    Validation er færdiggjort og klar til at blive testet. Der er skrevet i både opretbruger HTML, opretBruger js og inde i det samlede CSS dokument

--- OpretBruger.js
+++ OpretBruger.js
@@ -29,22 +29,73 @@ function kontrolInfo() {
     //Test af at det virker
     console.log(fornavn+efternavn+email+tlf+startTid+slutTid+brugernavn+kodeord);
 
-    //For at kontrollere input tilføjes to yderligere variabler
-    function validation(){
-        var formValid = true; alert("Revisor oprettet")
-        var validationMessage = "";
+    //For at funktionen kan køre, skal den køres. Det gør den her
+    kontrolInput();
 
-        if(formValid) {
+    //For at kontrollere input tilføjes denne function
+   function kontrolInput() {
+        var inputCorrect = true;
 
-        } else {
-            alert(validationMessage)
+       //Her kontrolleres indholdet
+       if (fornavn==null || fornavn=="") {
+           //Hvis dette gør sig gældende, påkaldes det id som er givet i HTML dokumentet
+           document.getElementById("fejlFornavn").innerHTML = "Indtast fornavn";
+           inputCorrect = false;
+       }
+
+       if (efternavn==null || efternavn=="") {
+           document.getElementById("fejlEfternavn").innerHTML = "Indtast efternavn";
+           inputCorrect = false;
+       }
+
+       //Ved kontrol af korrekt email skal @ og . eksistere. Punktummet skal være efter @
+       //Ved at bruge .indexof metoden, kan man tælle hvor et givent tegn befinder sig.
+       //Derfor skal @>1 og punktum >2
+       //For at bruge den her indexof metode, skal der laves to variabler
+
+       var snabelA = email.indexOf("@");
+       var punktum = email.indexOf(".");
+
+       if (email==null || email=="" || snabelA<1 || punktum<2) {
+           document.getElementById("fejlEmail").innerHTML = "Indtast korrekt email";
+           inputCorrect = false;
+       }
+
+       //Da HTML siden er lavet sådan at der kun kan indtastes tal, er det ikke nødvendigt at definere her.
+       //Et tlf nr skal være på minimum 8 tal og under 10 tal (i tilfælde af at de skriver +45)
+       //Derfor skal nr ligge mellem 10 mio og 1 mia
+       if (tlf==null || tlf<=10000000 || tlf>=1000000000) {
+           document.getElementById("fejlTlf").innerHTML = "Indtast korrekt tlf nr.";
+           inputCorrect = false;
+       }
+       //Det skal ikke være muligt at have kunder fra 1400-0800 f.eks. derfor skal start tid
+       //være mindre end sluttiden. Der refereres til de tidligere definerede variabler
+       //Inde i HTML, er det defineret at man ikke kan taste et tal som er større end 24
+       if (startTime>slutTime) {
+           document.getElementById("fejlTid").innerHTML = "Indtast korrekt tid";
+           inputCorrect = false;
+       }
+
+       //Kontrol af brugernavn
+       if (brugernavn==null || brugernavn=="") {
+           document.getElementById("fejlBrugernavn").innerHTML = "Indtast brugernavn";
+           inputCorrect = false;
+       }
+
+       //Kontrol af kodeord
+       if (kodeord==null || kodeord=="") {
+           document.getElementById("fejlKodeord").innerHTML = "Indtast kodeord";
+           inputCorrect = false;
+       }
+
+
+        //Her påkaldes den besked som skal dukke op, ved korrekt udfyldelse
+        if(inputCorrect) {
+            alert("Revisor oprettet");
         }
-        return (form_valid);
 
-        if (fornavn == null){
-            document.getElementById("fejlFornavn") = "Indtast fornavn"
-        }
     }
+
 
 
     saveToDB();
