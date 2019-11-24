3936f33c6a05449d62e91af7a2e8eb03e1acc1d0
marzamustafa <mamu17ad@student.cbs.dk>
2019-11-24 12:32:40 +0100

    Siden informerer en om, at der ingen møder er + Optimering af slet knap + andre små rettelser

--- revisorLoginside.js
+++ revisorLoginside.js
@@ -1,14 +1,14 @@
 
-var id = sessionStorage.getItem('loggedInRevisorId');
+var roid = sessionStorage.getItem('loggedInRevisorId');
 var ro = new Array( JSON.parse (sessionStorage.getItem('loggedInRevisorObject')) );
 
-if (id == null || ro == null) {
+if (roid == null || ro == null) {
     location.href = "Login.html";
 }
 
 ro = formaterRevisor(ro)[0];
 
-console.log(id);
+console.log(roid);
 console.log(ro);
 
 //Henter og indsætter info om hvilken revisor der er logget ind
@@ -39,13 +39,14 @@ document.getElementById('dag').addEventListener('change', myfunc);
 myfunc();
 
     function myfunc() {
-
+        document.getElementById("mødeoversigt").innerHTML = "";
         år = document.getElementById("år").value;
         måned = document.getElementById("måned").value;
         dag = document.getElementById("dag").value;
 
 
-        var valgtDato = new Date (år, måned, dag)
+        var valgtDato = new Date (år, måned, dag);
+        var erDerMøder = false;
 
         for (var i=0; i<møder.length; i++){
             console.log(møder[i].getStartTid());
@@ -54,6 +55,8 @@ myfunc();
             var startTid = møder[i].getStartTid();
             var slutTid = møder[i].getSlutTid();
             var mødeDato = new Date (startTid.getFullYear(), startTid.getMonth(), startTid.getDate());
+
+
 
             //if-statement som siger, at hvis mødedato lig valgtdato, så udskriver den mødeobjektet
             if (valgtDato.getFullYear() == mødeDato.getFullYear() &&
@@ -65,50 +68,57 @@ myfunc();
                 var startTid = møder[i].getStartTid();
                 var slutTid = møder[i].getSlutTid();
                 var id = møder[i].getID();
+                erDerMøder = true;
                 console.log(kundenavn);
                 console.log(kommentar);
                 console.log(mail);
                 console.log(tlfnr);
                 console.log(startTid);
 
-            //Gør mødestart/slut læseligt
-            startTid = startTid.toLocaleTimeString();
-            slutTid = slutTid.toLocaleTimeString();
+                //Gør mødestart/slut læseligt
+                startTid = startTid.toLocaleTimeString().substring(0,5);
+                slutTid = slutTid.toLocaleTimeString().substring(0,5);
 
-            //Laver en variabel for div'en "mødeoversigt
-            var mødeoversigt =  document.getElementById("mødeoversigt");
+                //Laver en variabel for div'en "mødeoversigt
+                var mødeoversigt = document.getElementById("mødeoversigt");
 
-            //Skaber et element til møderne for den dag, hvor kundens informationer indsættes i HTML
-            var møde = document.createElement("div");
-                møde.innerHTML = "Kundenavn: " + kundenavn + "<br />" + mail + "<br />" + tlfnr + "<br />" + startTid + " - " + slutTid + "<br />" + "Yderligere kommentar: " + kommentar + "<br />" + "<button id='sletMøde' onclick='sletMøde("+id+")>Slet Møde</button>";
+                //Skaber et element til møderne for den dag, hvor kundens informationer indsættes i HTML
+                var møde = document.createElement("div");
+                møde.innerHTML = "Kundenavn: " + kundenavn + "<br />" + mail + "<br />" + tlfnr + "<br />" + startTid + " - " + slutTid + "<br />" + "Yderligere kommentar: " + kommentar + "<br />" + "<button id='sletMøde' onclick='sletMøde(" + id + ")'>Slet Møde</button>";
+                møde.classList = "enkelteMøde";
                 mødeoversigt.appendChild(møde);
 
-                function sletMøde(id) {
-
-                    for (var i=0; i<ro.getMøder().length; i++){
-                       if (ro.getMøder() [i].getID() == id)  {
-                           var møderArray = ro.getMøder();
-                           møderArray.pop(i);
-
-                           //Find revisor som er logget ind, og erstat hans møder med møderArray
-                           //Til sidst gem hele revisorhuset igen i gemtRevisorhus
-
-                           break;
-
-                       }
-
-                    }
 
             }
-            
         }
 
-
+        if(!erDerMøder) document.getElementById("mødeoversigt").innerHTML = 'Der er ingen møder denne dag :(';
     }
 
 
-        
+
+function sletMøde(id) {
+
+    for (var i = 0; i < ro.getMøder().length; i++) {
+        if (ro.getMøder() [i].getID() == id) {
+            var møderArray = ro.getMøder();
+            møderArray.pop(i);
+            var grh = getGemtRevisorHus();
+            grh.getRevisorer()[roid].setMøder(møderArray);
+
+            localStorage.setItem('gemtRevisorhus', JSON.stringify(grh));
+            sessionStorage.setItem('loggedInRevisorObject', JSON.stringify(grh.getRevisorer()[roid]));
+
+            //Find revisor som er logget ind, og erstat hans møder med møderArray
+            //Til sidst gem hele revisorhuset igen i gemtRevisorhus
+            myfunc();
+
+            break;
+
+        }
+
     }
+}
 
 
 function logAf(){
